2b6c2546234cdb97459814ccfd3224a84728fd45
andre monteiro <124000838+AndreMonteiro225@users.noreply.github.com>
2026-07-20 06:01:50 -0300

    site pronto para a nuvem

--- src/lib/db.js
+++ src/lib/db.js
@@ -5,12 +5,15 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false // Isso avisa ao MySQL2 que a conexão deve ser segura
+  },
   waitForConnections: true,
-  connectionLimit: 20,
+  connectionLimit: 10,
   queueLimit: 0
 });
 
-// Teste automático de conexão
 pool.getConnection()
   .then(() => console.log("✅ CONECTADO AO MYSQL COM SUCESSO!"))
   .catch((err) => console.error("❌ ERRO AO CONECTAR NO MYSQL:", err.message));
